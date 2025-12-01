698856fae900ab07c0cae7f1dee6a30d2f15a872
Joshua Mark Castillo <joshuamarkcastillo0319@gmail.com>
2025-12-01 12:04:46 +0800

    Preparation for Web App Deployment.

--- back_end/src/server.js
+++ back_end/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
@@ -10,17 +11,31 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+// for deplmnt
+const __dirname = path.resolve();
 
-// middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-); // call this before running express and rate limiter in order for the front-end to respond beforehand
+if (process.env.NODE_ENV !== "production") {
+  // middleware for cross-site dev
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+// call this before running express and rate limiter in order for the front-end to respond beforehand
 app.use(express.json()); // So we can read the JSON
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+// For deplmnt
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../front_end/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../front_end", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(5001, () => {
--- front_end/src/lib/axios.js
+++ front_end/src/lib/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// If app is in production, there is no localhost so this will make the URL dynamic
+// based on the hosting provider (render, vercel etc.)
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
 });
 
 export default api;
